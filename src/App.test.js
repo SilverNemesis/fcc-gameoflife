1295cdbc6817d3f15342a2385173e7ce06652bf2
Donald Fries <donald.darklight@gmail.com>
2019-06-01 16:21:20 -0500

    added mock canvas to test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,51 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+function mockCanvas(window) {
+    window.HTMLCanvasElement.prototype.getContext = function () {
+        return {
+            fillRect: function () { },
+            clearRect: function () { },
+            getImageData: function (x, y, w, h) {
+                return {
+                    data: new Array(w * h * 4)
+                }
+            },
+            putImageData: function () { },
+            createImageData: function () { return [] },
+            setTransform: function () { },
+            drawImage: function () { },
+            save: function () { },
+            fillText: function () { },
+            restore: function () { },
+            beginPath: function () { },
+            moveTo: function () { },
+            lineTo: function () { },
+            closePath: function () { },
+            stroke: function () { },
+            translate: function () { },
+            scale: function () { },
+            rotate: function () { },
+            arc: function () { },
+            fill: function () { },
+            measureText: function () {
+                return { width: 0 }
+            },
+            transform: function () { },
+            rect: function () { },
+            clip: function () { },
+        }
+    }
+
+    window.HTMLCanvasElement.prototype.toDataURL = function () {
+        return ""
+    }
+}
+
 it('renders without crashing', () => {
+    const window = document.defaultView
+    mockCanvas(window)
+
     const div = document.createElement('div')
     ReactDOM.render(<App />, div)
 })
